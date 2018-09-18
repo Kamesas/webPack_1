a46cb070f051611ba9a5c118680f706e0b9b24d1
Kamesas <semakaleksandr2014@gmail.com>
2018-09-18 17:27:11 +0300

    some chnge

--- src/index.js
+++ src/index.js
@@ -3,5 +3,12 @@ import "./js/let";
 import "./js/const";
 import "./js/spread";
 import "./js/templates";
+import "./js/classes";
 
 import "./css/style.sass";
+
+// [ae] addEventListener
+// [ga] getAttribute
+// [ih] innerHTML
+// [qsa] querySelectorAll
+// [afn] anonymous function
---
+++ src/js/classes.js
@@ -0,0 +1,13 @@
+/* class Test {}
+let test = new Test();
+console.log(typeof test); //obj
+console.log(test instanceof Test); //true
+ */
+
+class Task {
+  constructor() {
+    console.log(1);
+  }
+}
+
+let task = new Task();
